8023db225d6542742a9f215badc4d93a0c6df960
gioquiroz <giovanny.quiroz94@gmail.com>
2023-11-22 23:39:18 -0500

    Se organiza la división por 0

--- src/division/division.js
+++ src/division/division.js
@@ -1,9 +1,9 @@
 function division(num1, num2){
+    if(num1 === 0 || num2 === 0 && typeof num1 != "number" ||  typeof num2 != "number"){
+        return "Operación infinita no definida";
+    }
     if(typeof num1 != "number" ||  typeof num2 != "number"){
         return "Operación invalida";
-    }
-    if(num1 === 0 || num2 === 0 && typeof num1 != "number" ||  typeof num2 != "number"){
-        return "Operación indefinida";
     }
     return num1 / num2;
 }
--- src/division/division.test.js
+++ src/division/division.test.js
@@ -1,22 +1,20 @@
-const multiplicación = require("../multiplicación/multiplicación");
 const division = require("./division")
-
-test('60 / 4 should be 15', () => {
-    const result = division(60,4);
-    expect(result).toBe(15);
-});
-test('Si los numeros ingresados no son enteros debe mostrar un error', () => {
-    const error = "Operación invalida";
-    const result = division("60","4");
+test('24 / 0 no tiene resultado  definido', () => {
+    const result = division(24,0);
+    expect(result).toBe(Infinity)
+})
+test('Si uno de los numero es igual a 0 la operación debe ser indefinida', () => {
+    const error = "Operación infinita no definida";
+    const result = division("24","0");
     expect(result).toBe(error);
 });
 
-test('24 / 0 no tiene resultado  definido', () => {
-    const result = division(24,0);
-    expect(result).toBe(undefined)
-})
-test('Si uno de los numero es igual a 0 la operación debe ser indefinida', () => {
-    const error = "Operación indefinida";
-    const result = division("24","0");
+test('72 / 4 should be 18', () => {
+    const result = division(72,4);
+    expect(result).toBe(18);
+});
+test('Si los numeros ingresados no son enteros debe mostrar un error', () => {
+    const error = "Operación invalida";
+    const result = division("72","4");
     expect(result).toBe(error);
 });
